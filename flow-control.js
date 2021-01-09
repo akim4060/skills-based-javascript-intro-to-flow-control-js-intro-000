78316846760a1f14a1c2bd6d0e907d053acfbc96
Alex <akim4060@gmail.com>
2021-01-09 23:45:54 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -26,7 +26,7 @@ function ternaryTeenager(age){
 function switchAge(age){
   switch(age){
     case 13:
-    return("You are a teenager");
+    console.log("You are a teenager");
     break;
     case 14:
     return("You are a teenager");
