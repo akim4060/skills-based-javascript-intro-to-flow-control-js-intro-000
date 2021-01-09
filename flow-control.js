b05c0aa0791bd0f3d45e95b4facf6f6676c4483d
Alex <akim4060@gmail.com>
2021-01-09 23:26:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -23,7 +23,7 @@ function ageChecker(age){
 function ternaryTeenager(age){
   return age>12 && age<20 ? "You are a teenager" : "You are not a teenager"
 }
-var age = "13"
+
 function switchage(age){
   switch(age){
     case "13":
