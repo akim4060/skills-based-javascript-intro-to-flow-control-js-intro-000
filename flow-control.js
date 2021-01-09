f83f6235d0a964fad10617dff0cf52e7922c9bde
Alex <akim4060@gmail.com>
2021-01-09 22:26:55 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -2,3 +2,10 @@ function basicTeenager(age){
   if (age>12 && age<20)
   return "You are a teenager!"
 }
+
+function teenager(age){
+  if (age>12 && age<20)
+    return "You are a teenager!"
+} else {
+  "You are not a teenager!"
+}
