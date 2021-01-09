248029ed2631c0695c2cee60ca358252773a2645
Alex <akim4060@gmail.com>
2021-01-09 23:45:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -46,9 +46,6 @@ function switchAge(age){
     case 19:
     return("You are a teenager");
     break;
-    case 20:
-    return("You are a teenager");
-    break;
     default:
     return("You have an age");
   }
